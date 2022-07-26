61778031e019f3b685689d0edbbda862479e0ca0
Robert Hadaway <rhadaway14@gmail.com>
2022-07-26 12:37:31 -0600

    first commit

---
+++ src/pages/dashboard.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import * as ROUTES from '../constants/routes';
+
+
+export default function Dashboard() {
+
+    return (
+        <h1>hello</h1>
+    )
+};
